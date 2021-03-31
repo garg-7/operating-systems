9f822280c46c68184a8af893429f68310d45daba
Devin Garg <garg.7@iitj.ac.in>
2021-03-31 17:53:20 +0530

    corrected exponential random generator

--- lab3/generator.c
+++ lab3/generator.c
@@ -17,6 +17,7 @@ int main(int argc, char *argv[])
         scanf("%d", &n);
         printf("Enter the mean of exponential distribution: ");
         scanf("%f", &mu);
+        // printf("%d %f", n, mu);
     }
  
     if (mu == 0)
@@ -26,14 +27,22 @@ int main(int argc, char *argv[])
     }
 
     int *arrivals = (int *)malloc(n * sizeof(int));
+    int *aGenerated = (int *)malloc(n * sizeof(int));
     int *priorities = (int *)malloc(n * sizeof(int));
     int *cpuBursts = (int *)malloc(n * sizeof(int));
 
     arrivals[0] = 0;
+    aGenerated[0] = 0;
     for (int i = 0; i < n; i++)
     {
-        if (i > 0)
-            arrivals[i] = arrivals[i - 1] + (int)(-1 * log((float)rand() / (float)RAND_MAX) / mu * 9 + 1);
+        if (i > 0){
+            float interArrival = (float)rand() / (float)RAND_MAX;
+            if (interArrival<exp(-1 * 10.0/mu))
+                aGenerated[i] = 10;
+            else
+                aGenerated[i] = (int)(-1 * log(interArrival) * mu);
+            arrivals[i] = arrivals[i - 1] + aGenerated[i];
+        }
         priorities[i] = rand() % (10 + 1 - 1) + 1;   // between 1 and 10
         cpuBursts[i] = rand() % (20 + 1 - 1) + 1;   // between 1 and 20
     }
@@ -42,6 +51,6 @@ int main(int argc, char *argv[])
     for (int i = 0; i < n; i++)
     {
         fprintf(f, "%d,%d,%d\n", arrivals[i], priorities[i], cpuBursts[i]);
-        fprintf(stdout, "%d,%d,%d\n", arrivals[i], priorities[i], cpuBursts[i]);
+        // fprintf(stdout, "%d,%d,%d\n", aGenerated[i], priorities[i], cpuBursts[i]);
     }
 }
